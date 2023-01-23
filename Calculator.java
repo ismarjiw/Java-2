a64f977ef6adc62d8849f6a494defc68b5c23831
Ismarji Workman <ismarjiw@protonmail.com>
2023-01-23 16:52:39 -0500

    second commit

--- Calculator.java
+++ Calculator.java
@@ -27,45 +27,52 @@ class Arithmetic {
 
 /** The calculator program. */
 public class Calculator {
+
+  public static String[] tokenize(String input){
+    String[] tokens = input.split(" ");
+    return tokens;
+  }
+  public static String extractOperator(String[] tokens){
+    String operator = tokens[0];
+    return operator; 
+  }
+  public static boolean shouldQuit(String operator){
+    boolean quit = operator.toLowerCase().equals("q");
+    return quit; 
+  }
+  public static Float[] extractNums(String[] tokens){
+    Float[] nums = new Float[2];
+    nums[0] = Float.parseFloat(tokens[1]);
+
+    if (tokens.length >= 3) {
+      nums[1] = Float.parseFloat(tokens[2]);
+    } else {
+      nums[1]= 0f;
+    }
+    return nums; 
+  }
+  
   public static void main(String[] args) {
-    // main while loop that drives the calculator 
+
     while (true) {
-      // prompt user to enter equation
+
       String input = getUserInput("Enter your equation:");
       if (input == null) {
-        // if user doesn't input anything, asks to enter again
+
         System.out.println("Please enter an equation.");
         continue;
       }
-      // splits "enter your equation" input into variable tokens
-      class helperFunctions{
-        public static String[] tokenize(String input){
-          String[] tokens = input.split(" ");
-          return tokens;
-        }
-        public static String extractOperator(String[] tokens){
-          String operator = tokens[0];
-          return operator; 
-        }
-      }
-      String[] tokens = input.split(" ");
-      // @ [0] if equation is q, will quit the program 
-      String operator = tokens[0];
-      if (operator.toLowerCase().equals("q")) {
+
+      String[] tokens = tokenize(input); 
+    
+      String operator = extractOperator(tokens); 
+      if (shouldQuit(operator)) {
         System.out.println("Quitting the program. Goodbye!");
         break;
       }
-      // recognizing first and second numbers as floats
-      Float num1, num2;
+      Float[] nums;
       try {
-        // grabs first number 
-        num1 = Float.parseFloat(tokens[1]);
-        // grabs second number 
-        if (tokens.length >= 3) {
-          num2 = Float.parseFloat(tokens[2]);
-        } else {
-          num2 = 0f;
-        }
+        nums = extractNums(tokens);
       } catch (ArrayIndexOutOfBoundsException e) {
         System.out.println("Error: enter at least 1 number.");
         continue;
@@ -77,31 +84,31 @@ public class Calculator {
       Float result;
       switch (operator) {
         case "+":
-          result = Arithmetic.add(num1, num2);
+          result = Arithmetic.add(nums[0], nums[1]);
           break;
         
         case "-":
-          result = Arithmetic.subtract(num1, num2);
+          result = Arithmetic.subtract(nums[0], nums[1]);
           break;
 
         case "*":
-          result = Arithmetic.multiply(num1, num2);
+          result = Arithmetic.multiply(nums[0], nums[1]);
           break;
 
         case "/":
-          result = Arithmetic.divide(num1, num2);
+          result = Arithmetic.divide(nums[0], nums[1]);
           break;
 
         case "%":
-          result = Arithmetic.mod(num1, num2);
+          result = Arithmetic.mod(nums[0], nums[1]);
           break;
 
         case "square":
-          result = Arithmetic.square(num1);
+          result = Arithmetic.square(nums[0]);
           break;
 
         case "cube":
-          result = Arithmetic.cube(num1);
+          result = Arithmetic.cube(nums[0]);
           break;
 
         default:
